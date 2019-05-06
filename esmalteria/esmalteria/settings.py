0d18c8c2c7e40104fd591822bea62a0d98a4e23f
Tadeu <tadeuif@hotmail.com>
2019-05-06 11:50:44 -0300

    Sucesso2!

--- esmalteria/esmalteria/settings.py
+++ esmalteria/esmalteria/settings.py
@@ -124,5 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
