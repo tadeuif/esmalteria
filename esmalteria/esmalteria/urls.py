91a6720c20a6014cbda9735daf6d8175ac791f22
Tadeu <tadeuif@hotmail.com>
2019-05-07 01:17:08 -0300

    changes

--- esmalteria/esmalteria/urls.py
+++ esmalteria/esmalteria/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core.views import *
+#from core.views import *
 from django.conf.urls import url
 from django.views.generic.base import TemplateView
 
