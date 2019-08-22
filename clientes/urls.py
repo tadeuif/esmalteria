afdc92ae8f718b8446c59885922ea78aa07c09b4
Tadeu <tadeuif@hotmail.com>
2019-08-22 01:31:55 -0300

    TUDO FUNCIONANDO! CHUPA MUNDO!

--- clientes/urls.py
+++ clientes/urls.py
@@ -3,7 +3,7 @@ from .views import lista_de_clientes,adicionar_cliente,editar_cliente,remover_cl
 urlpatterns = [
     path('',lista_de_clientes,name='lista_de_clientes'),
     path('adicionar_cliente/',adicionar_cliente,name='adicionar_cliente'),
-    path('editar_cliente/<int:id>',editar_cliente),
-    path('remover_cliente/<int:id>',remover_cliente)
+    path('clientes/editar_cliente/<int:id>',editar_cliente,name='editar_cliente'),
+    path('clientes/remover_cliente/<int:id>',remover_cliente,name='remover_cliente')
     
 ]
