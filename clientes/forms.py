c771903aef25f900500c2221d7c264770882cbd3
Tibério Cruz <tibacruz@gmail.com>
2019-08-23 11:18:54 -0300

    Nois no Vale

--- clientes/forms.py
+++ clientes/forms.py
@@ -4,9 +4,10 @@ from .models import Cliente
 class ClienteForm(forms.ModelForm):
     class Meta:
         model =  Cliente
-        fields = ['nome','email','cpf']
+        fields = ['nome','email','cpf','redes']
         widgets = {
             'nome':forms.TextInput(attrs={'placeholder':'Informe o nome do cliente'}),
             'email':forms.TextInput(attrs={'placeholder':'Informe o email do cliente'}),
-            'cpf':forms.TextInput(attrs={'placeholder':'Informe o cpf do cliente'})
+            'cpf':forms.TextInput(attrs={'placeholder':'Informe o cpf do cliente'}),
+            'redes':forms.TextInput(attrs={'placeholder':'Informe a rede social do cliente'})
         }
