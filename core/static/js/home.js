6669423f1ec7c7d1b096659aeb9079fb205efbfd
Tadeu <tadeuif@hotmail.com>
2019-09-22 16:28:30 -0300

    Menu funcionando!

--- core/static/js/home.js
+++ core/static/js/home.js
@@ -1,3 +1,10 @@
+console.log('Hello!')
+
 function toggleSidebar(){
     document.getElementById("sidebar").classList.toggle('active');
+}
+
+function mudaText(){
+    var texto = document.getElementById("teste")
+    texto.textContent = "TESTE"
 }
