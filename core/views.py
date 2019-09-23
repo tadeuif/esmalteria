99cb79f9957e0e21f6946a2e0a99220bef2090d0
Tadeu <tadeuif@hotmail.com>
2019-09-23 00:45:52 -0300

    Aniversariantes quase pronto

--- core/views.py
+++ core/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.urls import reverse
-
+from clientes.models import Cliente
+from clientes.forms import ClienteForm
+import datetime
 
 def index(request):
     context = {
@@ -10,11 +12,19 @@ def index(request):
     }
     return render(request, "login.html", context)
 
+
+#def home(request):
+    #context = {
+        #'title': 'BEM VINDO'
+    #}
+    #return render(request, "home.html", context)
+#TENTANDO ACESSAR O BANCO(CASO DE PARA IMPORTAR, IMPORTE!)
 def home(request):
-    context = {
-        'title': 'BEM VINDO'
-    }
-    return render(request, "home.html", context)
+    clientes = Cliente.objects.all().order_by('-id')
+    mydate = datetime.datetime.now()
+
+    context = {'clientes':clientes, 'mydate':mydate}
+    return render(request,"home.html",context)
 
 def teste(request):
     context = {
